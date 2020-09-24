1339e86b8006e4ac27d847eaa08759e99d08fcad
root <root@ip-172-31-37-231.us-east-2.compute.internal>
2020-09-24 15:06:37 +0000

    new commit

---
+++ index.js
@@ -0,0 +1,13 @@
+const http = require('http');
+const  exec = require('child_process').exec;
+const port = 3000;
+http.createServer(function(req,res){
+	   exec("ps aux",(err,stdout,stderr)=>{
+	    res.write(stdout);
+		   res.end();
+	   })
+	   //res.write("Nodejs is working ;)");
+	   //res.end();
+    
+   
+}).listen(port);
